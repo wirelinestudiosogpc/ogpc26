d8bb2574a44a97ccc17198ad571336774a21b703
Garrett Kaiser <gkaiser5888@it01-125031.ad.ppsnet>
2026-02-10 15:29:04 -0800

    Re-did previous work

---
+++ Assets/Scripts/LungeHitbox.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class LungeHitbox : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Hit Player");
+            playerMovement.HP -= 1.5f;
+        }
+        else{
+            Debug.Log("Failed");
+        }
+    }
+}
